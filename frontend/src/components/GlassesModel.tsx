0c513accca941403913c3ddc1b093dc1de30e99a
g4ntavya <k13rohilla@gmail.com>
2026-02-08 22:29:26 +0530

    Fix TypeScript error breaking Vercel build

--- frontend/src/components/GlassesModel.tsx
+++ frontend/src/components/GlassesModel.tsx
@@ -1,6 +1,5 @@
 import { useRef, useEffect, forwardRef, useImperativeHandle } from 'react';
 import { useGLTF } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 
 export interface GlassesModelHandle {
