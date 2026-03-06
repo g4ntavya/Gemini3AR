555c3bcc83c9eb76c964124438d9be94f423f668
g4ntavya <k13rohilla@gmail.com>
2026-03-06 10:51:34 +0530

    updated package

--- frontend/src/components/RegistrationModal.tsx
+++ frontend/src/components/RegistrationModal.tsx
@@ -169,7 +169,6 @@ export function RegistrationModal({
                             placeholder="Name"
                             value={name}
                             onChange={e => setName(e.target.value)}
-                            autoFocus
                         />
 
                         <input
